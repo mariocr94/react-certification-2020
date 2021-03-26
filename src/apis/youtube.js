ac53c5fcac653a9c0f13a19c7fe697e4f985c3c6
mariocr94 <marioacr94@gmail.com>
2021-03-25 23:31:11 -0600

    Fixed api key secret

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-const KEY = 'AIzaSyBs1t9KiLbciuopk8CWMVJMimlGT7OgcxU';
+
+const KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
 
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3/',
